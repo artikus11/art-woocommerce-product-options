eb0aa85e6b03b55b50c8e5de9131c138d556c3ca
Артем <artikus.sol@gmail.com>
2025-01-08 01:08:14 +0400

    fix(awop 1.0.0): правки webpack

--- webpack.config.js
+++ webpack.config.js
@@ -100,6 +100,8 @@ module.exports = {
 						loader:  'sass-loader',
 						options: {
 							sourceMap: ! isProduction,
+							api: 'modern',
+							implementation: require('sass'),
 						},
 					},
 				],
