98f9e7f7bd674ea69c4eb77992ccda780f32c418
Артем <artikus.sol@gmail.com>
2025-01-09 22:08:13 +0400

    fix(awop 1.1.1): version & readme

--- art-woocommerce-product-options.php
+++ art-woocommerce-product-options.php
@@ -5,7 +5,7 @@
  * Text Domain: art-woocommerce-product-options
  * Domain Path: /languages
  * Description: Плагин для WooCommerce. добавляет дополнтельные опции к товару
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: Artem Abramovich
  * Author URI: https://wpruse.ru/
  * License: GPL-2.0+
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 const AWPO_PLUGIN_DIR    = __DIR__;
 const AWPO_PLUGIN_AFILE  = __FILE__;
-const AWPO_PLUGIN_VER    = '1.1.0';
+const AWPO_PLUGIN_VER    = '1.1.1';
 const AWPO_PLUGIN_NAME   = 'Art WooCommerce Product Options';
 const AWPO_PLUGIN_SLUG   = 'art-woocommerce-product-options';
 const AWPO_PLUGIN_PREFIX = 'awpo';
